1b6fcd953332da916868b98297a45c1014ba591b
VictorGutierrez <vagutierrezv@uqvirtual.edu.co>
2023-05-29 14:56:20 -0500

    Algunos tipos de datos.

--- 1. Introduccion/Tipos.py
+++ 1. Introduccion/Tipos.py
@@ -19,9 +19,20 @@ print(mensaje)
 numero_uno = -1
 print(numero_uno)
 
+#Int 2
+#Al dividir tenemos un numero flotante
+# // sirve para redondear numero entero
+numeroUno = 10 // 3
+print(numeroUno)
+
 #--------------------------------------
 #Float
 numero_dos = -3.14;
 print(numero_dos);
 #--------------------------------------
 #Booleano
+valor = True;
+print(valor)
+
+valor = False;
+print(valor)
