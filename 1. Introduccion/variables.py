5f734e56810efdb4cd186d28f5d990f744d4aac3
VictorGutierrez <vagutierrezv@uqvirtual.edu.co>
2023-05-29 14:48:08 -0500

    Inicio Curso Python

---
+++ 1. Introduccion/variables.py
@@ -0,0 +1,17 @@
+#<nombre> = <valor>
+
+titulo_curso = 'Curso Profesional de Python';
+
+# Una Variable se cambia en tiempo de ejecuciom.
+# curso = 'Curso Python';
+
+nombre_completo = 'Victor Alfonso Gutierrez Vasquez'
+
+print(titulo_curso);
+
+print(nombre_completo);
+
+
+print(nombre_completo+': '+titulo_curso);
+
+
