512896b9ac0f231a56585a8d00465a367765a0ef
VictorGutierrez <vagutierrezv@uqvirtual.edu.co>
2023-05-29 16:39:36 -0500

    Listas

---
+++ 1. Introduccion/lista.py
@@ -0,0 +1,12 @@
+# lista = ['String', 10, 16.6, True] # list() cualquiera para declarar un arreglo
+
+# print('Lista: ',lista)
+
+lista_cursos = ['Python', 'Django', 'Flask', 'Java']
+print('lista_cursos: ',lista_cursos)
+
+primer_curso = lista_cursos[0]
+print('primer_curso: ',primer_curso+'\n')
+
+for item in lista_cursos:
+ print(item)
