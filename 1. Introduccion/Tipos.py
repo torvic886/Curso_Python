90d4468589c2a082d3287c00339c705dbfb7549c
VictorGutierrez <vagutierrezv@uqvirtual.edu.co>
2023-05-29 14:49:05 -0500

    Tipos de Datos

---
+++ 1. Introduccion/Tipos.py
@@ -0,0 +1,27 @@
+#String
+titulo_curso = 'Curso Profesional de Python'
+print(titulo_curso);
+
+nombre_completo = "Eduardo Ismael"
+print(nombre_completo)
+
+mensaje = '"Código Facilito"'
+print(mensaje)
+
+mensaje = '''Te encuentras
+en el curso : Profesional de Python.
+En CódigoFacilito'''
+
+print(mensaje)
+
+#--------------------------------------
+#Int
+numero_uno = -1
+print(numero_uno)
+
+#--------------------------------------
+#Float
+numero_dos = -3.14;
+print(numero_dos);
+#--------------------------------------
+#Booleano
